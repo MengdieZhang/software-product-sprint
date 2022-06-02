321a8b72bee0def5c72ffd716df5d699582807ba
MengdieZhang <bell_mandy@outlook.com>
2022-06-02 16:23:55 +0000

    add basic functions

--- portfolio/README.md
+++ portfolio/README.md
@@ -1,3 +1,5 @@
+This repo contains Mengdie ZHang's portfolio and SPS projects
+
 This directory is where you'll write all of your code!
 
 By default it contains a barebones portfolio web app. To run a local server,
--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -15,14 +15,19 @@
 /**
  * Adds a random greeting to the page.
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+// source:https://github.com/google/software-product-sprint/blob/main/walkthroughs/week-1-web-development/examples/stanley/src/main/webapp/script.js
+function randomizeImage() {
+  // The images directory contains 5 images, so generate a random index between
+  // 1 and 5.
+  const imageIndex = Math.floor(Math.random() * 5) + 1;
+  const imgUrl = 'images/'+imageIndex + '.jpg';
 
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+  const imgElement = document.createElement('img');
+  imgElement.src = imgUrl;
 
   // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+  const imageContainer = document.getElementById('random-image-container');
+  // Remove the previous image.
+  imageContainer.innerHTML = '';
+  imageContainer.appendChild(imgElement);
 }
