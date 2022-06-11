774d27f19aa573b07dbbd727fadcb598a6f1d225
MengdieZhang <bell_mandy@outlook.com>
2022-06-11 06:58:59 +0000

    Add a function for leaving message

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -31,3 +31,13 @@ function randomizeImage() {
   imageContainer.innerHTML = '';
   imageContainer.appendChild(imgElement);
 }
+
+
+/** Fetches the current date from the server and adds it to the page. */
+async function showServerTime() {
+    const responseFromServer = await fetch('/date');
+    const textFromResponse = await responseFromServer.text();
+  
+    const dateContainer = document.getElementById('date-container');
+    dateContainer.innerText = textFromResponse;
+  }
