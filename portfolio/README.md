727d3c8e31faefb99dc43db4cc39fbb11b54d072
MengdieZhang <bell_mandy@outlook.com>
2022-06-16 06:41:01 +0000

    Store message to server

--- portfolio/README.md
+++ portfolio/README.md
@@ -1,3 +1,3 @@
-This repo contains Mengdie ZHang's portfolio and SPS projects
+This repo contains Mengdie Zhang's portfolio and SPS projects
 
 The portfolio websit is https://mzhang-sps-summer22.appspot.com/
--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -5,6 +5,12 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.FullEntity;
+import com.google.cloud.datastore.KeyFactory;
+import org.jsoup.Jsoup;
 
 /** Servlet that processes text. */
 @WebServlet("/form-handler")
@@ -15,12 +21,22 @@ public class FormHandlerServlet extends HttpServlet {
 
     // Get the value entered in the form.
     String textValue = request.getParameter("text-input");
+    long timestamp = System.currentTimeMillis();
 
     // Print the value so you can see it in the server logs.
     System.out.println("You submitted: " + textValue);
 
+    // Store data
+    Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+    KeyFactory keyFactory = datastore.newKeyFactory().setKind("Task");
+    FullEntity taskEntity = Entity.newBuilder(keyFactory.newKey())
+                        .set("title",textValue)
+                        .set("timestamp", timestamp)
+                        .build();
+    datastore.put(taskEntity);
+
     // Write the value to the response so the user can see it.
-    response.getWriter().println("You submitted: " + textValue);
+    //response.getWriter().println("You submitted: " + textValue);
 
   }
 }
